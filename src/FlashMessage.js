e231442581ea9e875ea5ad9fb58bb60fc6cc9086
Scott Veltkamp <scott@joinspartan.com>
2020-10-08 14:05:37 -0400

    fix imports

--- src/FlashMessage.js
+++ src/FlashMessage.js
@@ -1,26 +1,6 @@
 import React, { Component } from "react";
 import Svg, {
-  Circle,
-  Ellipse,
-  G,
-  Text,
-  TSpan,
-  TextPath,
   Path,
-  Polygon,
-  Polyline,
-  Line,
-  Rect,
-  Use,
-  Image,
-  Symbol,
-  Defs,
-  LinearGradient,
-  RadialGradient,
-  Stop,
-  ClipPath,
-  Pattern,
-  Mask,
 } from 'react-native-svg';
 
 import { StyleSheet, TouchableWithoutFeedback, Platform, StatusBar, Animated, Image, Text, View } from "react-native";
